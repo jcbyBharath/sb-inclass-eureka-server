a14a4f52933360f68a8867ec27e3775d7fc42188
JCByBharath <stepintoIT9@gmail.com>
2024-04-10 18:32:09 -0400

    initial commit

---
+++ src/main/java/com/sb/inclass/project/sbinclasseurekaserver/SbInclassEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.sb.inclass.project.sbinclasseurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SbInclassEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbInclassEurekaServerApplication.class, args);
+	}
+
+}
